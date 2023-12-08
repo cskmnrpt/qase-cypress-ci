20dd828d14b2f29859bd968d58fce224cc795ec7
demo-cds <146315163+demo-cds@users.noreply.github.com>
2023-12-09 03:40:38 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      //require('cypress-qase-reporter/dist')(on, config);// implement node event listeners here
-    },
-    "supportFile": false
-  },
   reporter: 'cypress-qase-reporter',
   reporterOptions: {
     mode: "testops",
