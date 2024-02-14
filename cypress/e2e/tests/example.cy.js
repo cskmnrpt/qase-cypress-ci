1dd976dc6982a4e4168a712d111f93077f356695
demo-cds <146315163+demo-cds@users.noreply.github.com>
2024-02-14 23:06:37 +0530

    Update example.cy.js

--- cypress/e2e/tests/example.cy.js
+++ cypress/e2e/tests/example.cy.js
@@ -7,20 +7,6 @@ qase([1], it('Authorization', () => {
   )
 )
 
-it.skip('Gets, types and asserts', () => {
-    cy.visit('https://example.cypress.io');
-    cy.contains('type').click();
-    
-    // Should be on a new URL which includes '/commands/actions'
-    cy.url().should('include', '/commands/actions');
-
-    // Get an input, type into it and verify that the value has been updated
-    cy.get('.action-email')
-    .type('fake@email.com')
-    .should('have.value', 'unexpected@email.com');
-    }
-)
-
   it('Test that is auto-created by Cypress', () => {
     cy.visit('https://qase.io');
     cy.title().should('Modern TMS solution');
